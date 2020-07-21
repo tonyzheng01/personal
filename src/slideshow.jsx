33739edd30d0963ac32f3dc0914b354bd4d2347f
Tony Zheng <tony.zheng@uconn.edu>
2020-07-20 23:25:58 -0400

    feat: slideshow w/ fixed height photos and static position other elements

--- src/slideshow.jsx
+++ src/slideshow.jsx
@@ -6,15 +6,24 @@ import {
   AiOutlineArrowLeft,
   AiOutlineArrowRight,
 } from "react-icons/ai";
-import Kendrick from "./kendrick";
-import Tyler from "./tyler";
+import kendrick from "./photos/kendrick.jpg";
+import tyler from "./photos/tyler.jpg";
 
 class Slideshow extends Component {
   constructor() {
     super();
     this.state = {
       index: 0,
-      photos: [<Kendrick />, <Tyler />],
+      photos: [kendrick, tyler],
+      captions: [
+        [
+          "Kendrick Lamar",
+          "TDE Championship Tour",
+          "XFINITY Theater",
+          "6-7-2018",
+        ],
+        ["Tyler, The Creator", "IGOR Toor", "Agganis Arena", "9-10-2019"],
+      ],
     };
     this.handleKeyPress = this.handleKeyPress.bind(this);
     this.handleLeft = this.handleLeft.bind(this);
@@ -31,11 +40,15 @@ class Slideshow extends Component {
   }
 
   handleLeft() {
-    this.setState({ index: Math.abs(this.state.index - 1) });
+    this.setState({
+      index: Math.abs((this.state.index - 1) % this.state.photos.length),
+    });
   }
 
   handleRight() {
-    this.setState({ index: Math.abs(this.state.index + 1) });
+    this.setState({
+      index: Math.abs((this.state.index + 1) % this.state.photos.length),
+    });
   }
 
   componentDidMount() {
@@ -48,19 +61,32 @@ class Slideshow extends Component {
 
   render() {
     return (
-      <div className="w-full h-full flex flex-col items-center justify-center text-white text-opacity-50 font-light">
-        <Link to="/">
-          <IconContext.Provider value={{ size: 50 }}>
-            <button className="m-2">
-              <AiFillHome />
-            </button>
-          </IconContext.Provider>
-        </Link>
-        <div className="m-2">Keyboard navigation is supported!</div>
+      <div className="h-full text-center text-white text-opacity-50 font-light">
+        <div>
+          <Link to="/">
+            <IconContext.Provider value={{ size: 50 }}>
+              <button className="m-2">
+                <AiFillHome />
+              </button>
+            </IconContext.Provider>
+          </Link>
+          <div className="m-2">Keyboard navigation is supported!</div>
+        </div>
 
-        {this.state.photos[this.state.index % this.state.photos.length]}
+        <div className="flex justify-center photo-height">
+          <img
+            src={this.state.photos[this.state.index]}
+            alt={this.state.captions[0]}
+            className="h-full"
+          />
+        </div>
+        <ul className="list-none text-center">
+          {this.state.captions[this.state.index].map((c) => (
+            <li>{c}</li>
+          ))}
+        </ul>
 
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-center">
           <IconContext.Provider value={{ size: 40 }}>
             <button onClick={this.handleLeft}>
               <AiOutlineArrowLeft />
